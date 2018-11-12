7d1ef43e128417a65eced6dc7130af7aa509badb
allauren <allauren@student.42.fr>
2018-11-13 00:42:08 +0100

    better document now but still not that awesome

--- find_in_doc.py
+++ find_in_doc.py
@@ -3,8 +3,10 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 import re
 from docx.enum.text import WD_COLOR_INDEX
-import copy
-import inspect
+from docx import Document
+from docx.text.run import Font, Run
+from docx.shared import RGBColor
+
 
 def findWholeWord(w):
     return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
@@ -17,7 +19,7 @@ def tokenizer(line):
         word_list = line
     token = []
     for w in word_list:
-        token.append(ps.stem(w))
+        token.append([ps.stem(w), w])
     return token
 
 def check_words(file, words):
@@ -30,17 +32,19 @@ def check_words(file, words):
         nparagraph = newdoc.add_paragraph()
         font = nparagraph.add_run().font
         nparagraph.paragraph_format.alignment = paragraph.paragraph_format.alignment
+        nparagraph.paragraph_format.left_indent = paragraph.paragraph_format.left_indent
+        nparagraph.paragraph_format.right_indent = paragraph.paragraph_format.right_indent
+        nparagraph.paragraph_format.line_spacing = paragraph.paragraph_format.line_spacing
+        nparagraph.style.name = paragraph.style.name
         for word in tsentence :
             found = 0
             for fword in twords :
-                if fword == word:
+                if fword[0]== word[0]:
                     found = 1
             if found :
-                print('caca')
-                nparagraph.add_run(word + ' ').bold = True
-                print(font.highlight_color)
+                font.color.rgb = RGBColor(0x3f, 0x2c, 0x36)
+                nparagraph.add_run(word[1] + ' ').bold = True
                 font.highlight_color = WD_COLOR_INDEX.TURQUOISE
-                print(font.highlight_color)
             else :
-                nparagraph.add_run(word + ' ').bold = False
+                nparagraph.add_run(word[1] + ' ').bold = False
     newdoc.save('newdoc.docx')
